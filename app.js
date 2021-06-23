033702f77b0e4eec8e9aca57b4149a311fafcdba
Vojtěch Smutný <vutristo@gmail.com>
2021-06-23 21:22:57 +0200

    add drawTile

--- app.js
+++ app.js
@@ -1,41 +1,16 @@
 // Global variables
 var ctx, cvs
 
+
 // Main function
 function setGame() {
     cvs = document.getElementById('game');
     ctx = cvs.getContext('2d');
+    cvs.width  = mapSettings.tileSize * mapSettings.map[0].length;
+    cvs.height = mapSettings.tileSize * mapSettings.map.length;
 
     return cvs, ctx, mapSettings.render()
 }
-
-//Loader
-var Loader = {
-    images: {}
-};
-
-Loader.loadImage = function (key, src) {
-    var img = new Image();
-
-    var d = new Promise(function (resolve, reject) {
-        img.onload = function () {
-            this.images[key] = img;
-            resolve(img);
-        }.bind(this);
-
-        img.onerror = function () {
-            reject('Could not load image: ' + src);
-        };
-    }.bind(this));
-
-    img.src = src;
-    return d;
-};
-
-Loader.getImage = function (key) {
-    return (key in this.images) ? this.images[key] : null;
-};
-
 
 // Set the game
 let gameSettings = {
@@ -47,19 +22,19 @@ let gameSettings = {
 let mapSettings = {
     map : [
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [3,0,0,0,0,0,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+        [2,0,0,0,0,0,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
         [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
         [3,0,0,0,0,0,0,0,2,1,3,0,0,0,0,0,0,0,0,0,0,0,0,2],
         [3,0,0,0,0,0,0,0,2,1,1,1,3,0,0,0,0,0,0,0,0,0,0,2],
         [3,0,0,0,0,0,0,0,0,0,2,1,1,1,1,3,0,0,0,0,0,0,0,2],
-        [3,0,0,0,0,0,0,0,0,0,0,0,2,3,0,0,0,0,0,0,0,0,0,2],
+        [3,0,0,0,0,0,0,0,0,0,0,0,2,3,0,0,0,0,0,0,0,0,0,3],
         [3,0,0,0,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
         [3,0,2,1,1,1,3,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,1,1],
         [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,1,1,1,1],
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+        [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]
     ],
-    tileSize: 20,
-    imageAtlas: Loader.getImage('tiles.png'),
+    tileSize: 20    ,
+    imageAtlas: 'tiles.png',
 
     getMapDimensions: function() {
         let rows = this.map.length;
@@ -72,27 +47,41 @@ let mapSettings = {
     getTile: function(col, row) {
         return this.map[col][row]
     },
+    drawTile: function (x, y, w, h, color) { 
+        ctx.fillRect(x, y, w, h);
+        ctx.fillStyle = color;
+
+     },
 
     render: function(){
-        for (var c = 0; c < this.getMapDimensions()[0]; c++) {
-            for (var r = 0; r < this.getMapDimensions()[1]; r++) {
-
-              var tile = this.getTile(c, r);
-
-              if (tile !== 0) { // 0 => empty tile
+        for (var r = 0; r < this.getMapDimensions()[1]; r++) {
+            for (var c = 0; c < this.getMapDimensions()[0]; c++) {
                 
-                ctx.drawImage(
-                  this.imageAtlas, // image
-                  (tile - 1) * this.tileSize, // source x
-                  0, // source y
-                  this.tileSize, // source width
-                  this.tileSize, // source height
-                  c * this.tileSize, // target x
-                  r * this.tileSize, // target y
-                  this.tileSize, // target width
-                  this.tileSize // target height
-                );
-              }
+                let tile = this.getTile(r, c)
+                
+                let color = ""
+
+                if (tile == 0) {
+                    color = "red"
+                } else if (tile == 1) {
+                    color = "blue"
+                } else if (tile == 2) {
+                    color = "green"
+                } else {
+                    color = "black"
+                }
+                // todo c, r must start on 0
+                this.drawTile(
+                    c * this.tileSize,
+                    r * this.tileSize,
+                    this.tileSize,
+                    this.tileSize,
+                    color
+                )
+                
+                
+
+
             }
           }
     }
